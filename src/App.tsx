8fd17a378b3681cebeb156e4aeac805aa1b0ebbb
Manthan-mishra <manthan9481147615@gmail.com>
2023-05-19 21:33:59 +0530

    making basic components and done with routing of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { EuiProvider } from '@elastic/eui';
+import "@elastic/eui/dist/eui_theme_light.css";
+import { Route, Routes } from "react-router-dom";
+import CreateMeeting from "./pages/CreateMeeting";
+import Dashboard from "./pages/Dashboard";
+import JoinMeeting from "./pages/JoinMeeting";
+import Login from "./pages/Login";
+import Meeting from "./pages/Meeting";
+import MyMeetings from "./pages/MyMeetings";
+import OneOnOneMeeting from "./pages/OneOnOneMeeting";
+import VideoConference from "./pages/VideoConference";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EuiProvider>
+       <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/create" element={<CreateMeeting />} />
+            <Route path="/create1on1" element={<OneOnOneMeeting />} />
+            <Route path="/videoconference" element={<VideoConference />} />
+            <Route path="/mymeetings" element={<MyMeetings />} />
+            <Route path="/join/:id" element={<JoinMeeting />} />
+            <Route path="/meetings" element={<Meeting />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="*" element={<Login />} />
+          </Routes>
+    </EuiProvider>
   );
 }
 
-export default App;
+export default App
