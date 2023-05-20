fca7d1914e0e65f13768e5f3ca4270f5c8121a5d
Manthan-mishra <manthan9481147615@gmail.com>
2023-05-20 20:25:18 +0530

    completed login and logout functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,8 @@ import {
     EuiTextColor,
   } from "@elastic/eui";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { firebaseAuth } from "../utils/FirebaseConfig";
+import { signOut } from "firebase/auth";
 import { useAppSelector } from "../app/hooks";
 import { useDispatch } from "react-redux";
 
@@ -18,7 +20,13 @@ const Header = () => {
     const [breadCrumbs, setBreadCrumbs] = useState([{text:"Dashboard"}]);
     const [isResponsive, setIsResponsive] = useState(false);
     const dispatch = useDispatch();
-    const section = [{
+
+    const logout = () => {
+      signOut(firebaseAuth);
+    };
+
+    const section = [
+      {
         items: [
             <Link to="/">
               <EuiText>
@@ -28,8 +36,45 @@ const Header = () => {
               </EuiText>
             </Link>,
           ],
-    }];
-    const responsiveSection = [{
+    },
+    
+      {
+        items: [
+          <>
+            {userName ? (
+              <EuiText>
+                <h3>
+                  <EuiTextColor color="white">Hello, </EuiTextColor>
+                  <EuiTextColor color="#0b5cff">{userName}</EuiTextColor>
+                </h3>
+              </EuiText>
+            ) : null}
+          </>,
+        ],
+      },
+      {
+        items: [
+          <EuiFlexGroup
+            justifyContent="center"
+            alignItems="center"
+            direction="row"
+            style={{ gap: "2vw" }}
+          >
+             <EuiFlexItem grow={false} style={{ flexBasis: "fit-content" }}>
+            <EuiButtonIcon
+              onClick={logout}
+              iconType="lock"
+              display="fill"
+              size="s"
+              aria-label="logout-button"
+            />
+          </EuiFlexItem>
+        </EuiFlexGroup>,
+      ],
+      }  
+  ];
+    const responsiveSection = [
+      {
         items: [
             <Link to="/">
               <EuiText>
@@ -39,7 +84,12 @@ const Header = () => {
               </EuiText>
             </Link>,
           ],
-    }];
+    },
+    {
+
+    }
+  
+  ];
 
     useEffect(() => {
         if (window.innerWidth < 480) {
