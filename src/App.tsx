76283167739098ae03c8376142ff489965342513
Manthan-mishra <manthan9481147615@gmail.com>
2023-05-20 08:57:03 +0530

    working with login component using ui-elastic

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { EuiProvider } from '@elastic/eui';
 import "@elastic/eui/dist/eui_theme_light.css";
 import { Route, Routes } from "react-router-dom";
-import CreateMeeting from "./pages/CreateMeeting";
+// import CreateMeeting from "./pages/CreateMeeting";
 import Dashboard from "./pages/Dashboard";
-import JoinMeeting from "./pages/JoinMeeting";
+// import JoinMeeting from "./pages/JoinMeeting";
 import Login from "./pages/Login";
-import Meeting from "./pages/Meeting";
-import MyMeetings from "./pages/MyMeetings";
-import OneOnOneMeeting from "./pages/OneOnOneMeeting";
-import VideoConference from "./pages/VideoConference";
+// import Meeting from "./pages/Meeting";
+// import MyMeetings from "./pages/MyMeetings";
+// import OneOnOneMeeting from "./pages/OneOnOneMeeting";
+// import VideoConference from "./pages/VideoConference";
 
 
 const App = () => {
@@ -17,12 +17,12 @@ const App = () => {
     <EuiProvider>
        <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/create" element={<CreateMeeting />} />
-            <Route path="/create1on1" element={<OneOnOneMeeting />} />
-            <Route path="/videoconference" element={<VideoConference />} />
+            {/* <Route path="/create" element={<CreateMeeting />} /> */}
+            {/* <Route path="/create1on1" element={<OneOnOneMeeting />} /> */}
+            {/* <Route path="/videoconference" element={<VideoConference />} />
             <Route path="/mymeetings" element={<MyMeetings />} />
             <Route path="/join/:id" element={<JoinMeeting />} />
-            <Route path="/meetings" element={<Meeting />} />
+            <Route path="/meetings" element={<Meeting />} /> */}
             <Route path="/" element={<Dashboard />} />
             <Route path="*" element={<Login />} />
           </Routes>
